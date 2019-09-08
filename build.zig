554b6528ced3a8f9aebfb37c87a2f40e11a39752
Gustav Olsson <gustav128@gmail.com>
2019-09-08 14:40:33 +0200

    build on osx

--- build.zig
+++ build.zig
@@ -1,14 +1,32 @@
+const builtin = @import("builtin");
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("zig-sokol-test", null);
     exe.setBuildMode(mode);
-    exe.addCSourceFile("src/sokol.c", [_][]const u8{});
-    exe.addCSourceFile("src/clear-sapp.c", [_][]const u8{});
-    exe.linkSystemLibrary("c");
-    exe.linkSystemLibrary("X11");
-    exe.linkSystemLibrary("GL");
+
+    if (builtin.os == .linux) {
+        exe.linkSystemLibrary("c");
+        exe.linkSystemLibrary("X11");
+        exe.linkSystemLibrary("GL");
+
+        const c_args = [_][]const u8{};
+        exe.addCSourceFile("src/sokol.c", c_args);
+        exe.addCSourceFile("src/clear-sapp.c", c_args);
+    } else if (builtin.os == .macosx) {
+        exe.addFrameworkDir("/System/Library/Frameworks");
+        exe.linkFramework("Cocoa");
+        exe.linkFramework("OpenGL");
+        exe.enableSystemLinkerHack(); // Uses system linker instead of embedded ldd, I think...
+
+        const c_args = [2][]const u8{
+            "-ObjC",
+            "-fobjc-arc",
+        };
+        exe.addCSourceFile("src/sokol.c", c_args);
+        exe.addCSourceFile("src/clear-sapp.c", c_args);
+    }
     exe.install();
 
     const run_cmd = exe.run();
